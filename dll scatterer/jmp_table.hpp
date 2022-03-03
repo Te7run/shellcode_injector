d02632596d90a60b6a49fded4a9695e6795ea29d
Te7run <100861802+Te7run@users.noreply.github.com>
2022-03-03 17:58:18 +0100

    Update jmp_table.hpp
    removed unused code

--- dll scatterer/jmp_table.hpp
+++ dll scatterer/jmp_table.hpp
@@ -4,9 +4,6 @@
 #include "memory.hpp"
 
 namespace jmp_table {
-
-	uint8_t jmp_abs[] = { 0xff, 0x25, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-
 	struct jmp_entry
 	{
 		uint8_t shellcode[14];
@@ -28,21 +25,7 @@ namespace jmp_table {
 
 		//printf("[+] added jmp table at 0x%p jmp to 0x%p\n", base, address);
 	}
-
-	void add_small_jmp(uint64_t base, int8_t offset) {
-		uint8_t jmp_rel[] = { 0xeb, 0x00 };
-		std::memcpy(jmp_abs + 1, &offset, 1);
-		jmp_entry entry;
-		entry.base = base;
-		entry.size = 2;
-		std::memcpy(&entry, jmp_rel, 2);
-		table.push_back(entry);
-
-		//printf("[+] added small jmp to jmp table at 0x%p jmp to 0x%p\n", base, base + 2 + offset);
-	}
-
 	bool map_tables() {
-		// todo
 		for (auto& entry : table) {
 			//std::memcpy((void*)entry.base, entry.shellcode, entry.size);
 			memory::write_virtual_memory((void*)entry.base, entry.shellcode, entry.size);
@@ -52,6 +35,5 @@ namespace jmp_table {
 
 		return true;
 	}
-	//uint64_t add_entry();
 
 }
